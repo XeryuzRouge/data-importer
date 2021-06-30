56d9ef1b7c580550ae0b0dc097c1e35aa3c6d846
XeryuzRouge <gag@dynamic.tech>
2021-06-30 15:07:42 -0500

    Show total transaction revenue

--- spec/controllers/transactions_controller_spec.rb
+++ spec/controllers/transactions_controller_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe TransactionsController do
         post :create, params: { transactions: Rails.root.join('spec/fixtures/files/example.tab.xlsx') }
       end.to change(Transaction, :count)
     end
+
+    it 'shows total' do
+      post :create, params: { transactions: Rails.root.join('spec/fixtures/files/example.tab.xlsx') }
+
+      expect(response).to redirect_to('/transactions/new')
+      expect(flash[:alert]).to eq('Total de ingresos importados: 95.0$')
+    end
   end
 end
